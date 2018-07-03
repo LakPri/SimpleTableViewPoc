f39ce27528f7bbef2d880a6f932c3b696b4d00eb
Lakshmi <lakshmi@gowthams-mbp.lan>
2018-07-03 21:40:41 +0200

    Initial Commit

---
+++ SimpleTableViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTableViewController
+//
+//  Created by Lakshmi on 7/3/18.
+//  Copyright © 2018 Lakshmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTableViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTableViewController
+//
+//  Created by Lakshmi on 7/3/18.
+//  Copyright © 2018 Lakshmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
